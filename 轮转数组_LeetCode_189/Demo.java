c36067d6febd634624742d38843b98eafc02f2f2
Ma-Jiacheng <128767571+Ma-Jiacheng@users.noreply.github.com>
2023-03-28 16:27:20 +0800

    Delete 轮转数组_LeetCode_189 directory

--- 轮转数组_LeetCode_189/Demo.java
+++
@@ -1,9 +0,0 @@
-package 轮转数组_LeetCode_189;
-
-public class Demo {
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        Solution solution = new Solution();
-        solution.rotate(nums, 3);
-    }
-}
